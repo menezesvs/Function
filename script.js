2bc3d16e1fc93a31c1fd3a94bb596f69471e1901
menezesvs <aline.vsousa@hotmail.com>
2023-01-30 17:47:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log("Seja bem vindo ao meu site!")
+  
+  let myName = "Alinne"
+  function display(nome){
+    return nome
+  }
+  console.log(display("Alinne"))
+
+  console.log("-----------------------------------------------")
+
+  function diceInfo(){
+    console.log("Alinne")
+    console.log("24 anos")
+    console.log("Eclética")
+  }
+  diceInfo()
+
+  console.log("-----------------------------------------------")
+
+  function diceFilm(){
+    console.log("Alita: Anjo de combate")
+    console.log("Time (inception)")
+  }
+  diceFilm()
+
+  console.log("-----------------------------------------------")
+
+    function counter(number1,number2){
+    while(number1 < number2){
+      number1 = number1 + 3
+      console.log(number1)
+    }
+  }
+  counter(1,20)
+
+  console.log("-----------------------------------------------")
+
+let year = 24
+let isTrue = "você é menor de idade"
+let isFalse = "você é maior de idade"
+  if(year == true){
+    console.log(isTrue)
+  } else{
+    console.log(isFalse)
+  }
+
+  console.log("----------------------------------------------- fim.")  
